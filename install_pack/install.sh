83d6ecd796a2cbbeea30f0118456d959458ab911
Alexey <darklexi494@gmail.com>
2016-07-07 01:08:44 +0300

    Added build system

--- install_pack/install.sh
+++ install_pack/install.sh
@@ -5,8 +5,8 @@ SCRIPTS_DIR=scripts
 chmod +x $SCRIPTS_DIR/repo_packs.sh
 ./$SCRIPTS_DIR/repo_packs.sh
 
-chmod +x $SCRIPTS_DIR/subl_qt.sh
-./$SCRIPTS_DIR/subl_qt.sh
+chmod +x $SCRIPTS_DIR/distrib.sh
+./$SCRIPTS_DIR/distrib.sh
 
 chmod +x $SCRIPTS_DIR/lib_data.sh
 ./$SCRIPTS_DIR/lib_data.sh
--- install_pack/scripts/lib_data.sh
+++ install_pack/scripts/lib_data.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-INSTALL_DATA_DIR=/usr/local
-DATA_DIR=ev3_arm
+DATA_DIR=ev3c-lib
 
 # install lib/ and include/ dirs
-sudo cp -r $DATA_DIR $INSTALL_DATA_DIR
+sudo make install -C $DATA_DIR
 
 # install qt project template
-EV3_TEMPLATE=ev3_proj_template
-QT_DIR=$HOME/Qt
+# EV3_TEMPLATE=ev3_proj_template
+# QT_DIR=$HOME/Qt
 
-cp -r $EV3_TEMPLATE $QT_DIR/Tools/QtCreator/share/qtcreator/templates/wizards/projects
+# cp -r $EV3_TEMPLATE $QT_DIR/Tools/QtCreator/share/qtcreator/templates/wizards/projects
