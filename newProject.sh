8e8195c8d4b8eed399ec0061babf938e8350312b
Alexey <darklexi494@gmail.com>
2016-07-09 20:11:11 +0300

    [master]: Sublime template replaced to script

--- newProject.sh
+++ newProject.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 
+read -d '' main_text <<"EOF"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ( void )
+{
+	printf( "Hello World\n" );
+
+	return( 0 );
+}
+EOF
+
+read -d '' makefile_text <<"EOF"
+# 
+# Configurable area
+# 
+ip_robot_address := __ip_address
+execute_name := __project_name
+
+objects := $(patsubst %.c,%.o,$(wildcard *.c))
+
+cross_compiler_directory := /usr/arm-linux-gnueabi
+ev3c_directory := /usr/local/ev3c
+ev3cleti_lib_directory = /usr/local/ev3cleti
+build_directory := build
+
+library_directory := $(cross_compiler_directory)/lib $(ev3c_directory)/lib $(ev3cleti_lib_directory)/lib
+include_directory := $(cross_compiler_directory)/include $(ev3c_directory)/include $(ev3cleti_lib_directory)/include
+
+libraries := ev3c ev3cleti stdc++ 
+
+cflags := $(addprefix -I, $(include_directory)) -Wall -O3
+ldflags := $(addprefix -L, $(library_directory)) $(addprefix -l, $(libraries))
+
+CC := arm-linux-gnueabi-gcc
+LD := $(CC)
+
+execute_file := $(build_directory)/$(execute_name)
+
+all: $(build_directory) $(execute_file) 
+	
+$(execute_file): $(build_directory)/$(objects) 
+	$(LD) -o $@ $< $(ldflags)
+
+$(build_directory)/%.o : %.c
+	$(CC) $(cflags) -c $< -o $@
+
+$(build_directory):
+	mkdir -p $@
+
+clean:
+	rm -rf $(build_directory)
+
+install: all
+	scp $(execute_file) robot@$(ip_robot_address):~/$(execute_name)
+EOF
+
 echo -n "Enter project directory [default: $HOME]:"
 
 read project_directory
@@ -40,5 +97,10 @@ if [[ -z $robot_ip ]]; then
 fi
 echo "Robot ip: $robot_ip"
 
-cp ev3_template_subl/main.c $project_directory/$project_name
-sed "s/__project_name/$project_name/g; s/__ip_address/$robot_ip/g" "ev3_template_subl/Makefile" > "$project_directory/$project_name/Makefile"
+echo "$main_text" > $project_directory/$project_name/main.c
+makefile_text=${makefile_text/__ip_address/$robot_ip}
+makefile_text=${makefile_text/__project_name/$project_name}
+echo "$makefile_text" > $project_directory/$project_name/Makefile
+
+# cp ev3_template_subl/main.c $project_directory/$project_name
+# sed "s/__project_name/$project_name/g; s/__ip_address/$robot_ip/g" "ev3_template_subl/Makefile" > "$project_directory/$project_name/Makefile"
