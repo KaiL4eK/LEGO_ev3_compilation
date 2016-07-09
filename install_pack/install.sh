c95ab7a4f3795465f1f651b401c7a34885b1cc61
Alexey <darklexi494@gmail.com>
2016-07-09 20:35:43 +0300

    [master]: Deleted scripts, standalone install scrpit set

--- install_pack/install.sh
+++ install_pack/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-SCRIPTS_DIR=scripts
+# install cross-compiler, cmake
+sudo apt-get update && \
+sudo apt-get install -y gcc-arm-linux-gnueabi g++-arm-linux-gnueabi #cmake libcanberra-gtk-module gtk2-engines-pixbuf
 
-chmod +x $SCRIPTS_DIR/repo_packs.sh
-./$SCRIPTS_DIR/repo_packs.sh
+# install sublime text 3 - text editor
+SUBLIME_PACKAGE=sublime-text_build-3114_amd64.deb
 
-chmod +x $SCRIPTS_DIR/distrib.sh
-./$SCRIPTS_DIR/distrib.sh
+sudo dpkg -i $SUBLIME_PACKAGE
+cp EV3.sublime-build ~/.config/sublime-text-3/Packages/User
 
-chmod +x $SCRIPTS_DIR/lib_data.sh
-./$SCRIPTS_DIR/lib_data.sh
+make
+sudo make install
--- install_pack/scripts/repo_packs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# install cross-compiler, cmake
-sudo apt-get update && \
-sudo apt-get install -y gcc-arm-linux-gnueabi g++-arm-linux-gnueabi #cmake libcanberra-gtk-module gtk2-engines-pixbuf
