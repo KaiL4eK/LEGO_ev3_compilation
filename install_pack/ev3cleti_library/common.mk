d307050f45877a170c0ce0147189c9e67e5e8a9a
Alexey <darklexi4@mail.ru>
2016-07-14 17:47:54 +0300

    [issue4]: Added common file

---
+++ install_pack/ev3cleti_library/common.mk
@@ -0,0 +1,8 @@
+CC = arm-linux-gnueabi-gcc
+CXX = arm-linux-gnueabi-g++
+LD = $(CC)
+AR = arm-linux-gnueabi-ar
+
+cross_compiler_directory = /usr/arm-linux-gnueabi
+ev3cleti_lib_directory = $(HOME)/ev3cleti
+
