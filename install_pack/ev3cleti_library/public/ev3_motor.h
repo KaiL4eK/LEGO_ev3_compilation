15073ab1a61f4d809cc986c326833a0d39b492f9
Leti <leti@mail.ru>
2016-08-01 21:31:41 +0300

    [MultiThread]: fixed Motor function

--- install_pack/ev3cleti_library/public/ev3_motor.h
+++ install_pack/ev3cleti_library/public/ev3_motor.h
@@ -5,7 +5,7 @@ bool Motor_isConnected ( const char *motor_port );
 void Motor_run_timed ( const char *motor_port, long time_ms, int16_t power );
 void Motor_run_forever ( const char *motor_port, int16_t power );
 void Motor_stop ( const char *motor_port );
-void Motor_stor_all ( void );
+void Motor_stop_all ( void );
 int32_t Motor_get_position ( const char *motor_port);
 void Motor_reset_position(const char *motor_port);
 void Motor_run_to_rel_pos( const char* motor_port, int32_t pos, int8_t power  );
