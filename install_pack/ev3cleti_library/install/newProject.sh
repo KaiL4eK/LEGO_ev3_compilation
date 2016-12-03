fff982b081a803f95dacb92f7424a9b5ba675298
Alexey <darklexi494@gmail.com>
2016-12-03 18:12:59 +0300

    [refactor] New makefile application include

--- install_pack/ev3cleti_library/install/newProject.sh
+++ install_pack/ev3cleti_library/install/newProject.sh
@@ -24,41 +24,8 @@ read -d '' makefile_text <<"EOF"
 ip_robot_address = __ip_address
 execute_name = __project_name
 
-objects = $(patsubst %.c,%.o,$(wildcard *.c))
-
-cross_compiler_directory = /usr/arm-linux-gnueabi
 ev3cleti_lib_directory = $(HOME)/ev3cleti
-build_directory = build
-
-library_directory = $(cross_compiler_directory)/lib $(ev3cleti_lib_directory)/lib
-include_directory = $(cross_compiler_directory)/include $(ev3cleti_lib_directory)/include
-
-libraries_static = ev3cleti stdc++
-libraries_dynamic = pthread m
-cflags = $(addprefix -I, $(include_directory)) -Wall -O3 -std=gnu99
-ldflags = $(addprefix -L, $(library_directory)) -Wl,-Bstatic $(addprefix -l, $(libraries_static)) -Wl,-Bdynamic $(addprefix -l, $(libraries_dynamic))
-
-CC = arm-linux-gnueabi-gcc
-LD = $(CC)
-
-execute_file = $(build_directory)/$(execute_name)
-
-all: $(build_directory) $(execute_file) 
-	
-$(execute_file): $(build_directory)/$(objects) 
-	$(LD) -o $@ $< $(ldflags)
-
-$(build_directory)/%.o : %.c
-	$(CC) $(cflags) -c $< -o $@
-
-$(build_directory):
-	mkdir -p $@
-
-clean:
-	rm -rf $(build_directory)
-
-install: all
-	scp $(execute_file) robot@$(ip_robot_address):~/$(execute_name)
+include $(ev3cleti_lib_directory)/ev3cleti.mk
 EOF
 
 # ------------------------------------------------------------------------------------------------
